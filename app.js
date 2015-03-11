f96ea7e2e0d0c1aed320eaceb9af25dc18fde872
Miguel Chateloin <mchateloin@gmail.com>
2015-03-11 05:51:42 -0400

    First attempt. Dynamic echonest playlist seeded with banned artists from Spotify.

---
+++ app.js
@@ -0,0 +1,149 @@
+var express = require('express'); // Express web server framework
+var request = require('request'); // "Request" library
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+
+var spotify_client_id = 'dd2fe3ab80ba4853a0b3c6dd6c919e3d'; // Your client id
+var spotify_client_secret = '4902c49e25fa49d092989d579b0cd1c8'; // Your client secret
+var echonest_api_key = 'ING7YKMEMYCTJJHGT';
+var echonest_consumer_key = 'e45e7b50ea464e12e8b6dbde3070eb8f';
+var echonest_shared_secret = 'Le4kIQhOQ8SdyZ2Mk6Z7CA';
+
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+var generateRandomString = function(length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
+
+var stateKey = 'spotify_auth_state';
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'))
+    .use(cookieParser());
+
+app.get('/login', function(req, res) {
+
+    var state = generateRandomString(16);
+    res.cookie(stateKey, state);
+
+    // your application requests authorization
+    var scope = 'user-read-private user-read-email user-library-read playlist-read-private';
+    res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+        response_type: 'code',
+        client_id: spotify_client_id,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state
+    }));
+
+    console.log(querystring.stringify({
+        response_type: 'code',
+        client_id: spotify_client_id,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state
+    }));
+});
+
+app.get('/callback', function(req, res) {
+
+    // your application requests refresh and access tokens
+    // after checking the state parameter
+
+    var code = req.query.code || null;
+    var state = req.query.state || null;
+    var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+    if (state === null || state !== storedState) {
+        res.redirect('/#' +
+        querystring.stringify({
+            error: 'state_mismatch'
+        }));
+    } else {
+        res.clearCookie(stateKey);
+        var authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            form: {
+                code: code,
+                redirect_uri: redirect_uri,
+                grant_type: 'authorization_code'
+            },
+            headers: {
+                'Authorization': 'Basic ' + (new Buffer(spotify_client_id + ':' + spotify_client_secret).toString('base64'))
+            },
+            json: true
+        };
+
+        request.post(authOptions, function(error, response, body) {
+            if (!error && response.statusCode === 200) {
+
+                var access_token = body.access_token,
+                    refresh_token = body.refresh_token;
+
+                var options = {
+                    url: 'https://api.spotify.com/v1/me',
+                    headers: { 'Authorization': 'Bearer ' + access_token },
+                    json: true
+                };
+
+                // use the access token to access the Spotify Web API
+                request.get(options, function(error, response, body) {
+                    console.log(body);
+                });
+
+                // we can also pass the token to the browser to make requests from there
+                res.redirect('/#' +
+                querystring.stringify({
+                    access_token: access_token,
+                    refresh_token: refresh_token
+                }));
+            } else {
+                res.redirect('/#' +
+                querystring.stringify({
+                    error: 'invalid_token'
+                }));
+            }
+        });
+    }
+});
+
+app.get('/refresh_token', function(req, res) {
+
+    // requesting access token from refresh token
+    var refresh_token = req.query.refresh_token;
+    var authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        headers: { 'Authorization': 'Basic ' + (new Buffer(spotify_client_id + ':' + spotify_client_secret).toString('base64')) },
+        form: {
+            grant_type: 'refresh_token',
+            refresh_token: refresh_token
+        },
+        json: true
+    };
+
+    request.post(authOptions, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var access_token = body.access_token;
+            res.send({
+                'access_token': access_token
+            });
+        }
+    });
+});
+
+console.log('Listening on 8888');
+app.listen(8888);
---
+++ public/js/spotify-echonest-tools.js
@@ -0,0 +1,262 @@
+jQuery.ajaxSettings.traditional = true;
+
+function getConfig() {
+    return {
+        apiKey: "ING7YKMEMYCTJJHGT",
+        spotifySpace: "spotify",
+        echoNestHost: "http://developer.echonest.com/"
+    };
+}
+
+/* Tools for making working with the Spotify and Echo Nest APIs easier */
+function getSpotifyPlayButtonForPlaylist(title, playlist) {
+    var embed = '<iframe src="https://embed.spotify.com/?uri=spotify:trackset:PREFEREDTITLE:TRACKS" style="width:640px; height:520px;" frameborder="0" allowtransparency="true"></iframe>';
+    var tids = [];
+    playlist.forEach(function(song) {
+        var tid = fidToSpid(song.tracks[0].foreign_id);
+        tids.push(tid);
+    });
+    var tracks = tids.join(',');
+    var tembed = embed.replace('TRACKS', tracks);
+    tembed = tembed.replace('PREFEREDTITLE', title);
+    var li = $("<span>").html(tembed);
+    return $("<span>").html(tembed);
+}
+
+function chunk(arr, nEach){
+    var i, j, temparr, out = [];
+    for (i = 0, j = arr.length; i < j; i += nEach) {
+        temparr = arr.slice(i, i+nEach);
+        out.push(temparr)
+    }
+    return out;
+}
+
+/* converts full URI to just the simple spotify id */
+
+function fidToSpid(fid) {
+    var fields = fid.split(':');
+    return fields[fields.length - 1];
+}
+
+function getSpotifyPlayer(sessionId, inPlaylist, callback) {
+    var curSong = 0;
+    var audio = null;
+    var player = createPlayer();
+    window.playlist = [];
+    var sessionId = sessionId;
+
+    function addSpotifyInfoToPlaylist(list) {
+        var tids = [];
+        list.forEach(function(song) {
+            var tid = fidToSpid(song.tracks[0].foreign_id);
+            tids.push(tid);
+        });
+
+        $.getJSON("https://api.spotify.com/v1/tracks/", { 'ids': tids.join(',')})
+            .done(function(data) {
+                console.log('sptracks', tids, data);
+                data.tracks.forEach(function(track, i) {
+                    list[i].spotifyTrackInfo = track;
+                    console.log("trackinfo", list[i].spotifyTrackInfo);
+                });
+
+                console.log('addSpotifyInfoToPlaylist list', list);
+                var filtered = filterSongs(list);
+                console.log('addSpotifyInfoToPlaylist filtered', filtered);
+                playlist = playlist.concat(filtered);
+                console.log('addSpotifyInfoToPlaylist playlist', playlist);
+
+                //Some initialization only logic
+                if(curSong === 0) {
+                    showCurSong(false);
+                    callback(player);
+                }
+            })
+            .error( function() {
+                info("Whoops, had some trouble getting that playlist");
+            }) ;
+    }
+
+    function filterSongs(songs) {
+        var out = [];
+
+        function isGoodSong(song) {
+            return song.spotifyTrackInfo.preview_url != null;
+        }
+
+        songs.forEach(function(song) {
+            if (isGoodSong(song)) {
+                out.push(song);
+            }
+        });
+
+        return out;
+    }
+
+    function showSong(song, autoplay) {
+        $(player).find(".sp-album-art").attr('src', getBestImage(song.spotifyTrackInfo.album.images, 300).url);
+        $(player).find(".sp-title").text(song.title);
+        $(player).find(".sp-artist").text(song.artist_name);
+        audio.attr('src', song.spotifyTrackInfo.preview_url);
+        if (autoplay) {
+            audio.get(0).play();
+        }
+    }
+
+
+    function getBestImage(images, maxWidth) {
+        var best = images[0];
+        images.reverse().forEach(
+            function(image) {
+                if (image.width <= maxWidth) {
+                    best = image;
+                }
+            }
+        );
+        return best;
+    }
+
+    function showCurSong(autoplay) {
+        console.log('showCurSong', curSong, playlist[curSong]);
+        showSong(playlist[curSong], autoplay);
+    }
+
+    function nextSong() {
+        curSong++;
+
+        console.log('nextSong curSong to from', curSong - 1, curSong);
+        console.log('nextSong playlist', playlist);
+
+        if (curSong < playlist.length) {
+            showCurSong(true);
+        } else {
+            fetchNextTracks();
+        }
+    }
+
+    function fetchNextTracks(){
+        console.log("fetchNextTracks", "entering...")
+        var url = getConfig().echoNestHost + 'api/v4/playlist/dynamic/next';
+        $.getJSON(url, {
+                'api_key': getConfig().apiKey,
+                results: 1,
+                'session_id': sessionId,
+                '_': Math.floor(Date.now())
+            })
+            .done(function(data) {
+                //info("");
+                if (! ('songs' in data.response)) {
+                    //info("Can't find that artist");
+                } else {
+                    console.log('fetchNextTracks', data);
+                    addSpotifyInfoToPlaylist(data.response.songs);
+
+                    if(curSong >= playlist.length){
+                        fetchNextTracks();
+                    } else {
+                        showCurSong(true);
+                    }
+                }
+            })
+            .error( function() {
+                //info("Whoops, had some trouble getting that batch of songs");
+            }) ;
+    }
+
+
+    function prevSong() {
+        if (curSong > 0) {
+            curSong--;
+            showCurSong(true);
+        }
+    }
+
+    function togglePausePlay() {
+        console.log('tpp', audio.get(0).paused);
+        if (audio.get(0).paused) {
+            audio.get(0).play();
+        } else {
+            audio.get(0).pause();
+        }
+    }
+
+    function createPlayer() {
+        var main = $("<div class='sp-player'>");
+        var img = $("<img class='sp-album-art'>");
+        var info  = $("<div class='sp-info'>");
+        var title = $("<div class='sp-title'>");
+        var artist = $("<div class='sp-artist'>");
+        var controls = $("<div class='btn-group sp-controls'>");
+
+        var next = $('<button class="btn btn-primary btn-sm" type="button"><span class="glyphicon glyphicon-forward"></span></button>');
+        var prev = $('<button class="btn btn-primary btn-sm" type="button"><span class="glyphicon glyphicon-backward"></span></button>');
+        var pausePlay = $('<button class="btn btn-primary btn-sm" type="button"><span class="glyphicon glyphicon-play"></span></button>');
+
+
+        audio = $("<audio>");
+        audio.on('pause', function() {
+            var pp = pausePlay.find("span");
+            pp.removeClass('glyphicon-pause');
+            pp.addClass('glyphicon-play');
+        });
+
+        audio.on('play', function() {
+            var pp = pausePlay.find("span");
+            pp.addClass('glyphicon-pause');
+            pp.removeClass('glyphicon-play');
+        });
+
+        audio.on('ended', function() {
+            console.log('ended');
+            nextSong();
+        });
+
+        next.on('click', function() {
+            nextSong();
+        });
+
+        pausePlay.on('click', function() {
+            togglePausePlay();
+        });
+
+        prev.on('click', function() {
+            prevSong();
+        });
+
+
+        info.append(title);
+        info.append(artist);
+
+        controls.append(prev);
+        controls.append(pausePlay);
+        controls.append(next);
+
+        main.append(img);
+        main.append(info);
+        main.append(controls);
+
+        main.bind('destroyed', function() {
+            console.log('player destroyed');
+            audio.pause();
+        });
+        return main;
+    }
+
+    addSpotifyInfoToPlaylist(inPlaylist);
+    return player;
+}
+
+// set up a handler so if an element is destroyed,
+// the 'destroyed' handler is invoked.
+// See // http://stackoverflow.com/questions/2200494/jquery-trigger-event-when-an-element-is-removed-from-the-dom
+
+(function($){
+    $.event.special.destroyed = {
+        remove: function(o) {
+            if (o.handler) {
+                o.handler()
+            }
+        }
+    }
+})(jQuery);
