7c348727a83a0db0d57692af265e6af51c18087b
Miguel Chateloin <mchateloin@gmail.com>
2015-04-09 22:42:55 -0400

    Fixed to follow config npm package's conventions.

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ var generateRandomString = function(length) {
     }
     return text;
 };
-
+console.log('spotify config', config.util.getConfigSources());
+return;
 var spotifyKey = config.get('spotify.cookieName');
 
 var app = express();
